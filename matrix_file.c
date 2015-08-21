b738cb6394d7237e03f4504829586577fe70619c
Guilherme Henrique <gjhenrique@gmail.com>
2015-08-21 00:01:05 -0300

    Accepting blocks input as pure string also. Not just files

--- matrix_file.c
+++ matrix_file.c
@@ -4,11 +4,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "boilerplate.h"
 #include "block_list.h"
 
-struct Matrix *populate_matrix(FILE *file, int row_size, int col_size)
+
+struct Matrix *populate_matrix(char **saved_ptr, int row_size, int col_size)
 {
     char *line = NULL;
     size_t len = 0;
@@ -16,49 +20,49 @@ struct Matrix *populate_matrix(FILE *file, int row_size, int col_size)
     struct Matrix *matrix = create_matrix(row_size, col_size);
     for (int i = 0; i < matrix->row_size; ++i)
     {
-        getline(&line, &len, file);
+        line = strtok_r(NULL, "\n", saved_ptr);
         for(int j = 0, k = 0; k < matrix->col_size; j+=2, k++)
         {
             matrix->values[i][k] = to_digit(line[j]);
         }
-        free(line);
-        line = NULL;
     }
 
-   return matrix;
+    return matrix;
 }
 
-struct Matrix *read_matrix(FILE *file, char *line)
+struct Matrix *read_matrix(char **saved_ptr, char *line)
 {
-    int i, j, k, row_size, col_size;
+    int i, j, k, row_size;
 
-    if (strlen(line) != 2)
+    if (strlen(line) != 1)
     {
+      printf("%d\n", strlen(line));
         fprintf(stderr, "Line %s is malformed", line);
         exit(EXIT_FAILURE);
     }
 
     row_size = to_digit(line[0]);
 
-    return populate_matrix(file, row_size, row_size);
+    // Blocks should be a square matrix
+    return populate_matrix(saved_ptr, row_size, row_size);
 }
 
-struct BlockList *read_lines(FILE *file)
+struct BlockList *read_from_string(char blocks_string[])
 {
     char *line = NULL;
+    char *saved_ptr;
     size_t len = 0;
     ssize_t read;
 
     int color_index = 0;
     struct BlockList *block_list = create_list();
-
-    while ((read = getline(&line, &len, file)) != -1) {
-
-        if(!(strlen(line) == 1 || line[0] == '#'))
+    for(line = strtok_r(blocks_string, "\n", &saved_ptr); line != NULL; line = strtok_r(NULL, "\n", &saved_ptr))
+    {
+        if(!(strlen(line) == 0 || line[0] == '#' || !isdigit(line[0])))
         {
             struct Block *block = malloc(sizeof(struct Block));
 
-            block->matrix = read_matrix(file, line);
+            block->matrix = read_matrix(&saved_ptr, line);
 
             // Jumping color NONE
             if(color_index == NONE)
@@ -68,11 +72,6 @@ struct BlockList *read_lines(FILE *file)
 
             add(block_list, block);
         }
-    }
-
-    if (line)
-    {
-        free(line);
     }
 
     return block_list;
@@ -89,8 +88,15 @@ struct BlockList *read_from_file(char *file_name)
         fprintf(stderr, "Cannot read file %s", file_name);
     }
 
-    struct BlockList *block_list = read_lines(file);
+    int fd = fileno(file);
+    struct stat stat_file;
+    fstat(fd, &stat_file);
 
+    int size = stat_file.st_size;
+    char blocks_string[size +1];
+    fread(blocks_string, sizeof(char), size, file);
+
+    struct BlockList *block_list = read_from_string(blocks_string);
     fclose(file);
 
     return block_list;
--- matrix_file.h
+++ matrix_file.h
@@ -1,6 +1,8 @@
 #ifndef MATRIX_FILE_H
 #define MATRIX_FILE_H
 
+struct BlockList *read_from_string(char *blocks_string);
+
 struct BlockList *read_from_file(char *file_name);
 
 #endif
