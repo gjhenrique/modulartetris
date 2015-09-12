c175836184879eb04a271a7730867c3ed2b1d344
Guilherme Henrique <gjhenrique@gmail.com>
2015-09-12 12:54:58 -0300

    Improve ncurses support

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
-LDFLAGS=-g -lncurses -std=gnu99
+LDFLAGS=-g -std=gnu99
 
 all: main.c
 	gcc block_list.c matrix_file.c board.c boilerplate.c block.c main.c $(LDFLAGS) -o main
 
+ncurses:
+	gcc block_list.c matrix_file.c board.c boilerplate.c block.c ncurses.c $(LDFLAGS) -lncurses -lpanel -o ncurses
+
 test:
 	gcc block_list.c matrix_file.c board.c boilerplate.c block.c test/test.c test/greatest.h $(LDFLAGS) -o run_test
 
-.PHONY: all test
+.PHONY: all test ncurses
--- ncurses.c
+++ ncurses.c
@@ -1,67 +1,163 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <ncurses.h>
+#include <panel.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-#define WORLD_WIDTH 20
-#define WORLD_HEIGHT 40
+#include "model.h"
+#include "matrix_file.h"
+#include "boilerplate.h"
+#include "board.h"
 
-void print_rectangle(WINDOW *window, int x, int y, int height, int width)
+#define STEP 1000
+#define BOARD_WIDTH 20
+#define BOARD_HEIGHT 10
+
+struct NcursesGame
 {
-    int y_itr, x_itr;
-    for(y_itr = y; y_itr <= y + height; y_itr++)
+    WINDOW *board_window;
+    WINDOW *next_block_window;
+    PANEL *board_panel;
+    PANEL *next_block_panel;
+
+    struct Board *board;
+};
+
+void init_ncurses()
+{
+    initscr();
+    noecho();
+    cbreak();
+    halfdelay(5);
+    keypad(stdscr, true);
+    curs_set(false);
+    srand(time(NULL));
+
+    if(has_colors())
     {
-        for(x_itr = x; x_itr <= x + width; x_itr++)
+        start_color();
+        init_pair(RED, COLOR_RED, COLOR_RED);
+        init_pair(BLUE, COLOR_BLUE, COLOR_BLUE);
+        init_pair(GREEN, COLOR_GREEN, COLOR_GREEN);
+        init_pair(YELLOW, COLOR_YELLOW, COLOR_YELLOW);
+        init_pair(PINK, COLOR_CYAN, COLOR_CYAN);
+        init_pair(PURPLE, COLOR_MAGENTA, COLOR_MAGENTA);
+        init_pair(ORANGE, COLOR_YELLOW, COLOR_YELLOW);
+    }
+    else
+    {
+        fprintf(stderr, "[Your terminal does not support colors");
+        endwin();
+        exit(1);
+    }
+}
+
+struct NcursesGame *init_game()
+{
+
+    struct NcursesGame *game = malloc(sizeof(struct NcursesGame));
+    struct BlockList *blockList = read_from_file("default_blocks");
+    struct Board *board = create_board(10, 20, blockList);
+
+    game->board_window = newwin(board->height + 2, board->width + 2, 10, 10);
+    game->board_panel = new_panel(game->board_window);
+
+    game->board = board;
+
+    wclear(stdscr);
+    wclear(game->board_window);
+
+    return game;
+}
+
+void draw_board(struct NcursesGame *game)
+{
+    wclear(game->board_window);
+    box(game->board_window, false, false);
+
+    for(int i = 0; i < game->board->height; i++)
+    {
+        for(int j = 0; j < game->board->width; j++)
         {
-           	mvwprintw(window, y_itr, x_itr, " ");
+            if (game->board->visited[i][j] > 0)
+            {
+                int ch = ' ' | COLOR_PAIR(game->board->visited[i][j]);
+                mvwaddch(game->board_window, i + 1, j + 1, ch);
+            }
         }
     }
 }
 
+void free_game(struct NcursesGame *game)
+{
+    wclear(stdscr);
+    endwin();
+    free_board(game->board);
+    free(game);
+}
+
+bool route(struct NcursesGame *game, int ch)
+{
+    if (ch == 'q')
+    {
+        free_game(game);
+        endwin();
+        return false;
+    }
+
+    switch (ch)
+    {
+    case 'k': case KEY_UP:    rotate_anticlockwise(game->board); break;
+    case 'j': case KEY_DOWN:  next_move(game->board); break;
+    case 'h': case KEY_LEFT:  move_to_left(game->board) ; break;
+    case 'l': case KEY_RIGHT: move_to_right(game->board); break;
+    case ' ':                 move_to_bottom(game->board); break;
+    default: break;
+    }
+
+  return true;
+}
 
 int main(int argc, char *argv[])
 {
-
-    // Freeing Memory
-
-    /*struct Block *block = malloc(sizeof(struct Block));
-    block->row_size = 4;
-    block->col_size = 1;
-    block->format_bool = build_format_map(block->row_size, block->col_size);
-    block->col_sizeor = RED;
-
-    int i, j;
-
-    rotate_block_clockwise(&block);*/
-    /*
-    int i;
-
-    struct Block *block = (struct Block *) malloc(sizeof(struct Block));
-
-    initscr();
-    start_col_sizeor();
-    noecho();
-    curs_set(FALSE);
-    keypad(stdscr, TRUE);
-
-    init_pair(3,2,3);
-
-    WINDOW *board = newwin(WORLD_HEIGHT, WORLD_WIDTH, 3, 10);
-
-    int x = 10;
-    int y = 0;
-    for (i = 0; i < 10; ++i)
+    init_ncurses();
+    struct NcursesGame *game = init_game();
+    for(int i = 0; i < 10; i++)
     {
-        y++;
-        wclear(board);
-        box(board, 0, 0);
-        wattron(board, COLOR_PAIR(3));
-        print_rectangle(board, x, y, 1, 2);
-        wattroff(board, COLOR_PAIR(3));
-        usleep(500000);
-        wrefresh(board);
+        next_move(game->board);
     }
 
-    getchar();
-    delwin(board);
-    endwin();
-    */
+    struct timeval prev, now;
+    int diff;
+    gettimeofday(&prev, NULL);
+
+    // Game loop "stolen" from https://github.com/theabraham/terminal-tetris
+    while (route(game, getch())) {
+
+      gettimeofday(&now, NULL);
+      diff = ((now.tv_sec-prev.tv_sec)*1000)+((now.tv_usec-prev.tv_usec)/1000);
+      //if (diff >= game->step && (!game.ended && !game.paused)) {
+      if (diff >= STEP)
+      {
+          next_move(game->board);
+          gettimeofday(&prev, NULL);
+      }
+
+      // draw end screen if the game's over, otherwise draw our board (unless paused)
+      /*if ((game.ended = istopped())) {
+        drawend();
+      } else 
+        if (!game.paused) {
+        addscore(clrcols())*/
+
+      draw_board(game);
+
+      // reflect changes in our panels onscreen
+      update_panels();
+      doupdate();
+    }
+
     return 0;
 }
